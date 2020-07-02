f1791dd104a105e0c5a8bc9f88cdf1b5c73280da
Mandeep Sai <63707885+Mandeep-Sai@users.noreply.github.com>
2020-07-02 16:31:11 +0200

    swagger

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
-const express=require('express')
-const cors=require('cors')
+const express = require("express");
+const cors = require("cors");
+const YAML = require("yamljs");
+const swaggerUI = require("swagger-ui-express");
+const { join } = require("path");
 
-
+const swaggerDocument = YAML.load(join(__dirname, "./apiDesc.yml"));
 //Routes
-const productsRoute=require("./services/products")
-const reviewsRoutes = require('./services/reviews')
-const server=express()
-server.use(cors())
-server.use(express.json())
+const productsRoute = require("./services/products");
+const reviewsRoutes = require("./services/reviews");
+const server = express();
+server.use(cors());
+server.use(express.json());
 
-server.use("/products",productsRoute)
+server.use("/products", productsRoute);
 
+console.log(join(__dirname, "./apiDesc.yml"));
+server.use("/reviews", reviewsRoutes);
+server.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-server.use("/reviews",reviewsRoutes)
-
-server.listen(3006,()=>{
-    console.log("working on port 3006")
-})
+server.listen(3006, () => {
+  console.log("working on port 3006");
+});
